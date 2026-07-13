866510780b70ad6ea5f077e604c4ab7a8f630bdf
Christopher Bengen <cbengen84@gmail.com>
2026-07-13 16:54:00 -0400

    beginning postgresql conversion from JSON

---
+++ project/database/db_runner.py
@@ -0,0 +1,27 @@
+import psycopg
+
+conn = psycopg.connect(
+    dbname="library_db",
+    user="christopherbengen",
+    password="5401",
+    host="localhost",
+    port=5432
+)
+
+def _execute(query: str, params=None):
+    with conn.cursor() as cur:
+        cur.execute(query, params)
+    conn.commit()
+
+def _fetch_all(query: str, params=None):
+    with conn.cursor() as cur:
+        cur.execute(query, params)
+        return cur.fetchall()
+    
+def _fetch_one(query:str, params):
+    with conn.cursor() as cur:
+        cur.execute(query, params)
+        return cur.fetchone()
+    
+def _insert_book(title, author, publication, genre, holder):
+    pass
--- project/main.py
+++ project/main.py
@@ -1,4 +1,13 @@
 from system import main_menu
+"""
+User Login Credentials
+Emma -  User ID: 0
+Nelson - 
+Chris - 
+Jackson - 
+Pollock - ID: 498124 | ADMIN_ID: 983257
+"""
+
 
 if __name__ == "__main__":
     main_menu()
